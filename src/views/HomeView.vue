b6006cf1a04994b77a05ae620f19a0cc1fbb6d14
Stefanny <steflx0510@gmail.com>
2023-10-14 19:00:17 +0700

    home 2 done, otw setup lottie home-3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,35 +4,75 @@
       <Navbar />
     </nav>
 
-    <section class="home-1">
-      <div class="home-1-wrapper">
-        <img src="../assets/images/home/section1/helloworld.svg" alt="background-image" class="background-image-hero">
-        
-        <img src="../assets/images/home/section1/line.svg" alt="line-image" class="line-image-hero">
-
-        <div class="home-1-heroes-wrapper-0">
-          <div class="home-1-heroes-wrapper">
-            <div class="home-1-name yellow">STEFANNY</div>
-
-            <div class="home-1-heroes">
-              <h1 class="hero">UI / UX Designer with a sprinkle of Front-End Development skills</h1>
-              <p class="hero-content">I craft user-centric experience that blend usability, accessibility, and innovation driven by understanding of user & stakeholder needs aligning seamlessly with market demands and product quality. </p>
+    <section class="section-wrapper">
+      <section class="home-1">
+        <div class="home-1-wrapper">
+  
+          <img src="../assets/images/home/section1/helloworld.svg" alt="background-image" class="background-image-hero">
+          
+          <img src="../assets/images/home/section1/line.svg" alt="line-image" class="line-image-hero">
+  
+          <div class="home-1-heroes-wrapper-0">
+            <div class="home-1-heroes-wrapper">
+              <div class="home-1-name yellow">STEFANNY</div>
+  
+              <div class="home-1-heroes">
+                <h1 class="hero">UI / UX Designer with a sprinkle of Front-End Development skills</h1>
+                <p class="hero-content">I craft user-centric experience that blend usability, accessibility, and innovation driven by understanding of user & stakeholder needs aligning seamlessly with market demands and product quality. </p>
+              </div>
+  
+              <div class="home-1-cta-wrapper">
+                <button class="hire-me white"><router-link to="/contact">Hire Me</router-link></button>
+  
+                <img src="../assets/images/home/section1/or.svg" alt="background-image">
+                
+                <button class="view-projects white"><router-link to="/">View Projects</router-link></button>
+              </div>
+  
+              <span class="hero-filler"></span>
             </div>
-
-            <div class="home-1-cta-wrapper">
-              <button class="hire-me white"><router-link to="/contact">Hire Me</router-link></button>
-
-              <img src="../assets/images/home/section1/or.svg" alt="background-image">
-              
-              <button class="view-projects white"><router-link to="/">View Projects</router-link></button>
-            </div>
-
-            <span class="hero-filler"></span>
           </div>
+  
+          <img src="../assets/images/home/section1/heroimage1.svg" alt="hero image" class="hero-image">
         </div>
+      </section>
+  
+      <section class="home-2">
+        <div class="home-2-wrapper">
+          <img src="../assets/images/home/section2/image01.svg" alt="product design screenshot 1" class="home-2-product-image">
+          <img src="../assets/images/home/section2/image02.svg" alt="product design screenshot 2" class="home-2-product-image">
+          <img src="../assets/images/home/section2/image03.svg" alt="product design screenshot 3" class="home-2-product-image">
+          <img src="../assets/images/home/section2/image04.svg" alt="product design screenshot 4" class="home-2-product-image">
+          <img src="../assets/images/home/section2/image05.svg" alt="product design screenshot 5" class="home-2-product-image">
+        </div>
+      </section>
 
-        <img src="../assets/images/home/section1/heroimage1.svg" alt="hero image" class="hero-image">
-      </div>
+      <section class="home-3">
+        <section class="home-3-wrapper">
+          <h2 class="home-3-header">When Design Meet Code: Crafting Digital Magic</h2>
+
+          <div class="home-3-item-wrapper">
+            <div class="home-3-item">
+
+              <div class="home-3-item-text">
+                <h3 class="home-item-header">The Intersection of <br>
+                Design and Code
+                </h3>
+
+                <p class="home-item-content">Despite my design focus, exposure to diverse technical subjects, including Front-End development, enriches my capabilities in product design and development.                  
+                </p>
+              </div>
+            </div>
+
+            <div class="home-3-item"></div>
+            <div class="home-3-item"></div>
+          </div>
+        </section>
+      </section>
+    </section>
+
+    <section class="home-3">
+
     </section>
 
   </div>
@@ -47,7 +87,8 @@
 </style>
 
 <script>
-import Navbar from '@/components/NavbarView.vue'
+import Navbar from '@/components/NavbarView.vue';
+
 
 export default {
   name: 'HomeView',
@@ -55,4 +96,12 @@ export default {
     Navbar,
   },
 }
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= 75) {
+    document.querySelector('.home-2-wrapper').classList.add('scrolled');
+  } else {
+    document.querySelector('.home-2-wrapper').classList.remove('scrolled');
+  }
+});
 </script>
