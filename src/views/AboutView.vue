d0fcbec0310727b38076789f8563c436439f3a9d
Stefanny <steflx0510@gmail.com>
2023-10-29 13:38:21 +0700

    about 3 & 4 responsive done [1]

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -33,38 +33,41 @@
           <h2 class="about-3-values">My Principles, My Values, My Approach, You Name It</h2>
         </div>
 
-        <div class="about-3-item-wrapper">
-          <div class="about-3-item">
-            <small class="about-3-item-number">01</small>
-            <p class="about-3-item-title"><strong class="white weight-700">Collaborate and communicate.</strong> I believe both of those aspects define whether the project is successful or not. Create something together is always better.</p>
-          </div>
+        <div class="about-3-item-wrapper-0">
+          <div class="about-3-item-wrapper">
+            <div class="about-3-item">
+              <small class="about-3-item-number">01</small>
+              <p class="about-3-item-title"><strong class="white weight-700">Collaborate and communicate.</strong> I believe both of those aspects define whether the project is successful or not. Create something together is always better.</p>
+            </div>
 
-          <div class="about-3-item">
-            <small class="about-3-item-number">02</small>
-            <p class="about-3-item-title"><strong class="white weight-700">Kaizen.</strong> There is always room for improvement, no matter how big or small. I am always learning and looking for new ways to improve the user experience.</p>
-          </div>
+            <div class="about-3-item">
+              <small class="about-3-item-number">02</small>
+              <p class="about-3-item-title"><strong class="white weight-700">Kaizen.</strong> There is always room for improvement, no matter how big or small. I am always learning and looking for new ways to improve the user experience.</p>
+            </div>
 
-          <div class="about-3-item">
-            <small class="about-3-item-number">03</small>
-            <p class="about-3-item-title"><strong class="white weight-700">Empathize.</strong> I take the time to understand the needs and wants of my users, and I use this information to create designs that are both user-friendly and visually appealing.</p>
-          </div>
+            <div class="about-3-item">
+              <small class="about-3-item-number">03</small>
+              <p class="about-3-item-title"><strong class="white weight-700">Empathize.</strong> I take the time to understand the needs and wants of my users, and I use this information to create designs that are both user-friendly and visually appealing.</p>
+            </div>
 
-          <div class="about-3-item">
-            <small class="about-3-item-number">04</small>
-            <p class="about-3-item-title"><strong class="white weight-700">Flexibility.</strong> I understand that every user is different and that no design should be the same. I design flexible UI/UX to meet the unique needs of every user.</p>
-          </div>
+            <div class="about-3-item">
+              <small class="about-3-item-number">04</small>
+              <p class="about-3-item-title"><strong class="white weight-700">Flexibility.</strong> I understand that every user is different and that no design should be the same. I design flexible UI/UX to meet the unique needs of every user.</p>
+            </div>
 
-          <div class="about-3-item">
-            <small class="about-3-item-number">05</small>
-            <p class="about-3-item-title"><strong class="white weight-700">Attention to detail.</strong> The little things matter. I pay attention to every detail, from the typography to the color palette to the placement of every element on the screen.</p>
-          </div>
+            <div class="about-3-item">
+              <small class="about-3-item-number">05</small>
+              <p class="about-3-item-title"><strong class="white weight-700">Attention to detail.</strong> The little things matter. I pay attention to every detail, from the typography to the color palette to the placement of every element on the screen.</p>
+            </div>
 
-          <div class="about-3-item">
-            <small class="about-3-item-number">06</small>
-            <p class="about-3-item-title"><strong class="white weight-700">Design purposefully.</strong> Every design should have a purpose. I don't just design things because they look good. I design things because they solve a problem or make a user's life better.</p>
+            <div class="about-3-item">
+              <small class="about-3-item-number">06</small>
+              <p class="about-3-item-title"><strong class="white weight-700">Design purposefully.</strong> Every design should have a purpose. I don't just design things because they look good. I design things because they solve a problem or make a user's life better.</p>
+            </div>
+            
           </div>
-          
         </div>
+
         
       </div>
     </section>
